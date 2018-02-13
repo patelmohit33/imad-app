0e0b7a1bf47cde9cafabb4efaa320a9ae3590e42
youfortmp <36437112+youfortmp@users.noreply.github.com>
2018-02-14 00:52:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req, res) {
-     res.send(createtemplate(articleone));
+     res.send(createtemplate(article-one));
 });
 
 app.get('/article-two',function(req, res) {
